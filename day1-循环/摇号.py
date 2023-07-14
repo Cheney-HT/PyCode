bf667d6287183962806d26c0c5af2a0b6050dcac
Cheney <2717916092@qq.com>
2023-07-14 10:04:17 +0800

    Update 摇号.py

--- day1-循环/摇号.py
+++ day1-循环/摇号.py
@@ -10,10 +10,13 @@ while count<3:
         sum2="".join(random.sample(s,5))
         car_num=f"京{sum1}.{sum2}"
         car_nums.append(car_num)
-        print(car_num)
-    u_input=input("请输入你喜欢的车牌号：")
+        print(i+1,car_num)
+    u_input=input("请输入你喜欢的车牌号：").strip()
     if u_input in car_nums:
         print(f"恭喜您选择了【{u_input}】车牌号")
         exit("Good luck")
+    else:
+        print("不合法的选择。。。")
+    count+=1
 
 
