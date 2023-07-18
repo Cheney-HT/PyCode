dd86d3a03a479b8f3e28324c3299a2e6a216c3bb
Cheney <2717916092@qq.com>
2023-07-18 11:15:00 +0800

    Update 文本模式.py

--- day3-文件操作/文本模式.py
+++ day3-文件操作/文本模式.py
@@ -16,9 +16,9 @@ f.write("123\n")
 f.write("胡歌\n")
 f.close()
 f=open("aa","r",encoding="utf-8")
-f.readline() #读一行
-f.read() #读剩余的全部
+f.readline()  # 读一行
+f.read()  # 读剩余的全部
 f.close()
 f=open("aa","a",encoding="utf-8")
-f.write("周杰伦\n")  #文本末尾最佳
+f.write("周杰伦\n")  # 文本末尾最佳
 f.close()
