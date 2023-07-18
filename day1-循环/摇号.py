df6b5af00b40c25dd4e926363f6921c5f2f5dd87
Cheney <2717916092@qq.com>
2023-07-18 14:46:52 +0800

    Update 摇号.py

--- day1-循环/摇号.py
+++ day1-循环/摇号.py
@@ -1,22 +1,20 @@
 import random
 import string
 
-s=string.ascii_uppercase+string.digits
-count=0
-while count<3:
-    car_nums=[] #存储车牌号
+s = string.ascii_uppercase + string.digits
+count = 0
+while count < 3:
+    car_nums = []  # 存储车牌号
     for i in range(20):
-        sum1=random.choice(string.ascii_uppercase)
-        sum2="".join(random.sample(s,5))
-        car_num=f"京{sum1}.{sum2}"
+        sum1 = random.choice(string.ascii_uppercase)
+        sum2 = "".join(random.sample(s, 5))
+        car_num = f"京{sum1}.{sum2}"
         car_nums.append(car_num)
-        print(i+1,car_num) #加编号
-    u_input=input("请输入你喜欢的车牌号：").strip()
+        print(i + 1, car_num)  # 加编号
+    u_input = input("请输入你喜欢的车牌号：").strip()
     if u_input in car_nums:
         print(f"恭喜您选择了【{u_input}】车牌号")
         exit("Good luck")
     else:
         print("不合法的选择。。。")
-    count+=1
-
-
+    count += 1
