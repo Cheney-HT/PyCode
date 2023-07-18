e87d50fc3fc60f66cb7259adae73834f743fcf9d
Cheney <2717916092@qq.com>
2023-07-18 09:54:03 +0800

    1

---
+++ day3-文件操作/Test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+ -*- coding: utf-8 -*-
+Time    : 2023/7/18 9:45
+AuThor  : Administrator
+File    : Test.py
+Project : PycharmCode
+"""
+import sys
+print(sys.getdefaultencoding())
--- day3-文件操作/文本模式.py
+++ day3-文件操作/文本模式.py
@@ -6,14 +6,14 @@ AuThor  : Administrator
 File    : 文本模式.py
 Project : PycharmCode
 """
-f=open("aa",mode="w")
+f=open("aa","w",encoding="utf-8")
 f.write("123\n")
 f.write("胡歌\n")
 f.close()
-f=open("aa",mode="r")
+f=open("aa","r",encoding="utf-8")
 f.readline() #读一行
 f.read() #读剩余的全部
 f.close()
-f=open("aa",mode="a")
+f=open("aa","a",encoding="utf-8")
 f.write("周杰伦\n")  #文本末尾最佳
 f.close()
