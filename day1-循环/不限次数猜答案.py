05b4ebd6bc42428e5aca77c2b00ae324e4db2c82
Cheney <2717916092@qq.com>
2023-07-29 11:43:36 +0800

    1

--- day1-循环/不限次数猜答案.py
+++ day1-循环/不限次数猜答案.py
@@ -1,9 +1,9 @@
-count=0
-sum=33
-while count>=0:
-    count+=1
-    guess=int(input("请输入你要猜测数字："))
-    if guess==sum:
+count = 0
+num = 33
+while count >= 0:
+    count += 1
+    guess = int(input("请输入你要猜测数字："))
+    if guess == num:
         exit("恭喜你猜对了")
     else:
         print(f"第{count}次猜数，猜错了")
---
+++ day2-数据类型/三元运算.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# Created By CHT  
+"""
+编码     : encoding="utf-8"
+Time    : 2023/7/27 17:38
+File    : 三元运算.py
+Project : PycharmCode
+"""
+a = 1
+b = 2
+c = 3
+d = 4
+print(a if a > b else c if c > d else d)
